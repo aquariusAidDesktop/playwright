fd11d5511d00fa01d9c35716dc2d1da0a2554868
aquariusAidDesktop <aquarius_desktop@mail.ru>
2025-04-12 10:48:04 +0300

    bnmnmnn

--- src/spam/index.ts
+++ src/spam/index.ts
@@ -4,7 +4,7 @@ import { spamToUser } from "./spamToUser";
 export async function runSpam() {
   const page = await getBrowserPage();
 
-  const targets = ["@cristyman"];
+  const targets = ["@cristyman", "@pero_pr"];
 
   for (const username of targets) {
     await spamToUser(page, username, "Привет! Это тест");
